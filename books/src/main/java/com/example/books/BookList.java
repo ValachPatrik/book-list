65739352827e98decbfa9d8d3c065d6d4ff9cdb9
BacaPutera <valachpatrik@gmail.com>
2023-03-13 14:06:37 +0100

    books

--- books/src/main/java/com/example/books/BookList.java
+++ books/src/main/java/com/example/books/BookList.java
@@ -7,7 +7,7 @@ public class BookList {
     private List<Book> bookList;
 
     public BookList(){
-        bookList = new ArrayList<Book>(); {
+        this.bookList = new ArrayList<Book>(); {
             new Book(1,"fds","fds");
             new Book(2,"fds","fds");
             new Book(3,"fds","fds");
