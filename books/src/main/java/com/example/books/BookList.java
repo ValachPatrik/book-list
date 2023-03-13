46395ca095b2b0a2ceac26f210ef933c656ace0a
Michal Chrzanowski <ge84lol@mytum.de>
2023-03-13 14:09:56 +0100

    Getters

--- books/src/main/java/com/example/books/BookList.java
+++ books/src/main/java/com/example/books/BookList.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class BookList {
-    private List<Book> bookList;
+    private final List<Book> bookList;
 
     public BookList(){
         this.bookList = new ArrayList<Book>(); {
@@ -13,6 +13,8 @@ public class BookList {
             new Book(3,"fds","fds");
         };
     }
-     
 
+    public List<Book> getBookList() {
+        return this.bookList;
+    }
 }
--- books/src/main/java/com/example/books/BooksController.java
+++ books/src/main/java/com/example/books/BooksController.java
@@ -3,11 +3,14 @@ package com.example.books;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class BooksController {
 
     @GetMapping("/books")
-    public Book books(){
-        return new Book(1, "One", "Title");
+    public List<Book> books(){
+        BookList bookList = new BookList();
+        return bookList.getBookList();
     }
 }
